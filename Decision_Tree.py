d3e0db37329c39183704a17e2ca0e1784164348c
necro-wbj <d04220837@gm.tut.edu.tw>
2018-07-29 23:02:10 +0800

    backup

--- Decision_Tree.py
+++ Decision_Tree.py
@@ -1,9 +1,24 @@
 #! python3
-import math
+from sklearn.datasets import load_iris
+from sklearn import tree
+import graphviz
+iris = load_iris()
+clf = tree.DecisionTreeClassifier()
+print(iris.target)
+
+# clf = clf.fit(iris.data, iris.target)
+# dot_data = tree.export_graphviz(clf, out_file=None)
+# graph = graphviz.Source(dot_data)
+# graph.render("iris")
+# dot_data = tree.export_graphviz(clf, out_file=None,
+#                                 feature_names=iris.feature_names,
+#                                 class_names=iris.target_names,
+#                                 filled=True, rounded=True,
+#                                 special_characters=True)
+# graph = graphviz.Source(dot_data)
+# print(graph)
+# # def entropy(m, n):
+# #     return -m * math.log(m, 2) - n * math.log(n, 2)
 
 
-def entropy(m, n):
-    return -m * math.log(m, 2) - n * math.log(n, 2)
-
-
-print(entropy(2 / 5, 3 / 5))
+# # print(entropy(2 / 5, 3 / 5))
